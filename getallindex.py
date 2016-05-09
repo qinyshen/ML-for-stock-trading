8fa7175eb009962a18f018b29972125c5a9bf63c
张铭杰 <zhangmingjie@zhangmingjiedeMacBook-Pro.local>
2016-05-10 02:41:46 +0800

    add indexlist

--- getallindex.py
+++ getallindex.py
@@ -3,8 +3,9 @@ import os
 
 
 def get_all_index():
-    index_list = open('NYSE.txt')
+    index_list = open('note.txt')
+    print index_list
     indexs = [inst for inst in index_list.readlines()]
     print indexs
-    for eachindex in indexs:
-        google.get_google_finance_intraday(eachindex)
+    #for eachindex in indexs:
+        #google.get_google_finance_intraday(eachindex)
