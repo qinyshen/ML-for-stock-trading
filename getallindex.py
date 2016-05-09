6469768a4b1a89a4c02961f43f9b6550faeee8ba
Thelordofdream <574483817@qq.com>
2016-05-10 03:09:09 +0800

    5-10 first version

--- getallindex.py
+++ getallindex.py
@@ -1,11 +1,13 @@
 import google
-import os
-
 
 def get_all_index():
-    index_list = open('note.txt')
+    index_list = open('NYSE.txt')
     print index_list
-    indexs = [inst for inst in index_list.readlines()]
+    indexs = [inst.strip().split('\r') for inst in index_list.readlines()]
     print indexs
-    #for eachindex in indexs:
-        #google.get_google_finance_intraday(eachindex)
+    for eachindex in indexs:
+        print eachindex
+        google.get_google_finance_intraday(eachindex)
+
+
+get_all_index()
--- google.py
+++ google.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 """
 Retrieve intraday stock data from Google Finance.
 """
