92349a8ce2458c0658cd03d0543986625b13ebd5
Thelordofdream <574483817@qq.com>
2016-05-10 13:43:35 +0800

    second version

--- getallindex.py
+++ getallindex.py
@@ -1,12 +1,23 @@
-from google import get_google_finance_intraday
-
+import google
+import csv
+import os
 
 def get_all_index():
-    index_list = open('NYSE.txt')
-    print index_list
-    indexes = [inst.strip().split('\r') for inst in index_list.readlines()]
+    index_list1 = csv.reader(file('1.csv','rb'))
+    index_list2 = csv.reader(file('2.csv','rb'))
+    table1 = [line for line in index_list1]
+    table2 = [line for line in index_list2]
+    nrows1 = len(table1)
+    nrows2 = len(table2)
+    indexes = [0]*(nrows1+nrows2-2)
+    for i in range(nrows1-1):
+        indexes[i] = table1[i+1][0]
+    for i in range(nrows2-1):
+        indexes[i+nrows1-1] =table2[i+1][0]
+    if not os.path.exists('DATA'):
+        os.makedirs('DATA')
     for each_index in indexes:
-        get_google_finance_intraday(each_index)
+        google.get_google_finance_intraday(each_index)
 
 
 get_all_index()
