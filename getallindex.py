a6b473deb084646f5bd3dca1d875378cec88e70b
Thelordofdream <574483817@qq.com>
2016-05-10 14:01:01 +0800

    second version

--- getallindex.py
+++ getallindex.py
@@ -2,18 +2,19 @@ import google
 import csv
 import os
 
+
 def get_all_index():
-    index_list1 = csv.reader(file('1.csv','rb'))
-    index_list2 = csv.reader(file('2.csv','rb'))
+    index_list1 = csv.reader(file('1.csv', 'rb'))
+    index_list2 = csv.reader(file('2.csv', 'rb'))
     table1 = [line for line in index_list1]
     table2 = [line for line in index_list2]
-    nrows1 = len(table1)
-    nrows2 = len(table2)
-    indexes = [0]*(nrows1+nrows2-2)
-    for i in range(nrows1-1):
-        indexes[i] = table1[i+1][0]
-    for i in range(nrows2-1):
-        indexes[i+nrows1-1] =table2[i+1][0]
+    rows1 = len(table1)
+    rows2 = len(table2)
+    indexes = [0] * (rows1 + rows2 - 2)
+    for i in range(rows1 - 1):
+        indexes[i] = table1[i + 1][0]
+    for i in range(rows2 - 1):
+        indexes[i + rows1 - 1] = table2[i + 1][0]
     if not os.path.exists('DATA'):
         os.makedirs('DATA')
     for each_index in indexes:
