0b3dbf2114219e200e2a0068b74b9d020976b5b6
Thelordofdream <574483817@qq.com>
2016-05-23 14:55:29 +0800

    add sort method

---
+++ sort_out_stocks.py
@@ -0,0 +1,16 @@
+import re
+import sys
+import math
+import os
+import glob
+from collections import defaultdict
+import pymysql.cursors
+# Connect to the database
+connection = pymysql.connect(user='root', password='root',
+                             database='learnMySQL')
+
+cursor = connection.cursor()
+cursor.execute("select * from table1")
+results = cursor.fetchall()
+for re in results:
+    print re
