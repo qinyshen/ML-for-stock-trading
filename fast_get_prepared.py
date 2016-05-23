ae0b48cdd2a0f8704367556becbec50da32655cd
Thelordofdream <574483817@qq.com>
2016-05-24 02:29:01 +0800

    upload samples

---
+++ fast_get_prepared.py
@@ -0,0 +1,45 @@
+import re
+import sys
+import math
+import os
+import glob
+from collections import defaultdict
+import pymysql.cursors
+import csv
+
+
+def store_index_list(input_set, file_name):
+    fw = open(file_name, 'w')
+    fw.writelines(input_set)
+    fw.close()
+
+connection = pymysql.connect(user='root', password='root',
+                             database='tickets')
+
+
+cursor = connection.cursor()
+cursor.execute("create table Prepared(symbol varchar(20) NOT NULL, max DOUBLE, min DOUBLE, AMV DOUBLE, PRIMARY KEY(symbol));")
+connection.commit()
+
+cursor.execute("SHOW TABLES")
+indexes = cursor.fetchall()
+index_list = []
+
+for each_index in indexes:
+    if each_index == 'Prepared':
+        continue
+    print each_index[0]
+    cursor = connection.cursor()
+    cursor.execute("select max(open) from %s" % each_index[0])
+    max = cursor.fetchall()[0][0]
+    cursor.execute("select max(open) from %s" % each_index[0])
+    min = cursor.fetchall()[0][0]
+    cursor.execute("select max(open) from %s" % each_index[0])
+    AMV = cursor.fetchall()[0][0]
+    sql = "INSERT INTO `Prepared` (`symbol`, `max`, `min`, `AMV`) VALUES (%s, %s, %s, %s)"
+    cursor.execute(sql, (each_index[0], max, min, AMV))
+    connection.commit()
+    index_list.extend([each_index[0] + ' ' + str(max) + ' ' + str(min) + ' ' + str(AMV)])
+
+print index_list
+store_index_list(index_list,'index_list.txt')
